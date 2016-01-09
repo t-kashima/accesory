4394f7e37f5c20ad642524c68a29894ea76f6ae8
t-kashima <takumi.kashima@nanapi.co.jp>
2016-01-09 16:26:20 +0900

    remove the layout file

--- app/src/main/java/com/unuuu/feedback/SatelliteLifecycleExecutor.kt
+++ app/src/main/java/com/unuuu/feedback/SatelliteLifecycleExecutor.kt
@@ -1,29 +1,47 @@
 package com.unuuu.feedback
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.PixelFormat
-import android.graphics.Rect
-import android.view.LayoutInflater
-import android.view.View
 import android.view.WindowManager
+import android.widget.ImageView
 
 public class SatelliteLifecycleExecutor : MyActivityLifecycleCallbacks.LifecycleExecutor {
 
-    var view: View
+    var imageView: ImageView
     var windowManager: WindowManager
 
     public constructor(context : Context) {
         windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        view = LayoutInflater.from(context).inflate(R.layout.overlay, null)
-        view.setOnTouchListener(View.OnTouchListener { view, event ->
-            val rect = Rect()
-            view.getHitRect(rect);
-            return@OnTouchListener !rect.contains(event.x.toInt(), event.y.toInt())
-        })
+        imageView = ImageView(context)
+        imageView.scaleType = ImageView.ScaleType.FIT_CENTER
+        updateImage(context, R.drawable.suntv)
+    }
+
+    /**
+     * update the image using the drawable id.
+     *
+     * @param context context
+     * @param drawableId drawable id
+     */
+    public fun updateImage(context : Context, drawableId : Int) {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(context.resources, drawableId, options)
+        options.inJustDecodeBounds = false;
+        var scale : Int;
+        if (options.outWidth < options.outHeight) {
+            scale = options.outHeight / 40;
+        } else {
+            scale = options.outWidth / 40;
+        }
+        options.inSampleSize = scale;
+        val bitmap = BitmapFactory.decodeResource(context.resources, drawableId, options);
+        imageView.setImageBitmap(bitmap)
     }
 
     override fun onForeground() {
-        val params = WindowManager.LayoutParams(
+        val windowLayoutParams = WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_TOAST,
@@ -31,10 +49,10 @@ public class SatelliteLifecycleExecutor : MyActivityLifecycleCallbacks.Lifecycle
                         WindowManager.LayoutParams.FLAG_FULLSCREEN or
                         WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
                 PixelFormat.TRANSLUCENT)
-        windowManager.addView(view, params)
+        windowManager.addView(imageView, windowLayoutParams)
     }
 
     override fun onBackground() {
-        windowManager.removeView(view);
+        windowManager.removeView(imageView);
     }
 }
